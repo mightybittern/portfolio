e65defbf828cfbe1acb94129b38c6a1475c7fe76
Martynas Kalenda <martynjux@gmail.com>
2025-07-28 23:24:52 +0300

    Fix GitHub Pages deployment: Add base path /portfolio/ to vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import svgr from 'vite-plugin-svgr';
 
 export default defineConfig({
+  base: '/portfolio/',
   plugins: [
     svgr(), // For handling SVG files
   ],
